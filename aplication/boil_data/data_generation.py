f4b0d462c6904cc77bc69b74a1ead09ae596a714
Davi <davi.alves.fogarolii@gmail.com>
2024-08-19 09:14:56 -0300

    isotaleted create data function and publishers

---
+++ aplication/boil_data/data_generation.py
@@ -0,0 +1,28 @@
+import random
+
+data = ["Name ", "pressure", "temperature", "speed"]
+
+def sensor_data_generator():
+    sensor_data = [] 
+
+    for x in range(1):
+        dictionary = {}
+
+        pressure = random.randint(0, 20)
+        temperature = random.randint(0, 20)
+        speed = random.randint(0, 20)
+
+        dictionary = {data[0] : "Sensor "+str(x+1)}
+        dictionary[data[1]] = pressure
+        dictionary[data[2]] = temperature
+        dictionary[data[3]] = speed
+
+        sensor_data.append(dictionary)
+
+    return sensor_data
+#sensor_data[
+#   {"Name ": data, "pressure": data, "temperature": data, "speed": data},
+#   {"Name ": data, "pressure": data, "temperature": data, "speed": data},
+#   ]
+
+# print(sensor_data)
---
+++ aplication/boil_data/publisher_manual.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+from data_generation import sensor_data_generator
+import time 
+
+#sensor_data[
+#   {"Name ": data, "pressure": data, "temperature": data, "speed": data},
+#   {"Name ": data, "pressure": data, "temperature": data, "speed": data},
+#   ]
+
+sensor_data = sensor_data_generator()
+# print(sensor_data)
+for x in range(len(sensor_data)):
+    # print(sensor_data, "x ", x)
+    mqtt_client = mqtt.Client("Publishe "+sensor_data[0]["Name "])
+    mqtt_client.connect(host="localhost", port=1883)
+    mqtt_client.publish(topic="/sensor_data", payload=f'{sensor_data[x]}')
+    time.sleep(2)
+
+# for some reason need import time if we want to publish 2 diferent payload usin for structure before another publish
+    # import time 
+    # # time.sleep(2)
+    # mqtt_client = mqtt.Client("meu_publisher")
+    # mqtt_client.connect(host="localhost", port=1883)
+    # mqtt_client.publish(topic="/sensor_data", payload="{'minha: mensagem'}")

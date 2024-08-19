645de9f98610eb7f804e637f8422c9786fe05f6a
Davi <davi.alves.fogarolii@gmail.com>
2024-08-19 13:59:51 -0300

    creat a new main to isolate connection function

--- aplication_lib/postgresql_pkg/main.py
+++ aplication_lib/postgresql_pkg/main.py
@@ -1,34 +1,9 @@
 import psycopg2
-from config import postgres_config
+from connect import connect_database
 
 
-def connect_database():
-    #connect to the database
-    connection = None
-    
-    try:
-        params = postgres_config()
-        print("Connecting to the postgresSQL database ...")
-        connection = psycopg2.connect(**params)
-
-        #cursos objecti will help to create any queries on the database and retrieve data
-        cursor = connection.cursor()
-        print("PostgresSQL data base version: ")
-        cursor.execute("SELECT version()")
-    
-        database_version = cursor.fetchone()
-        print(database_version)
-
-        cursor.close()
-
-    except(Exception, psycopg2.DatabaseError) as error:
-        print("ERROR IN CONNECTION: ", error)
-    
-    finally:
-        if connection is not None:
-            connection.close()
-            print("Database connection terminated")
-
+def main():
+    connect_database()
 
 if __name__ == "__main__":
-    connect_database()
+    main()

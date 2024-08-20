152bac8f39c43b89f91ba67913b112f484e31f24
Davi <davi.alves.fogarolii@gmail.com>
2024-08-20 12:01:23 -0300

    incremente de function o main file

--- postgresql_pkg/main.py
+++ postgresql_pkg/main.py
@@ -1,9 +1,13 @@
-import psycopg2
-from connect import connect_database
-
+from connect_db import connect_database, disconnect_database
+from insert import insert
+from create_table import create_table
 
 def main():
-    connect_database()
+    connection, cursor = connect_database()
+    
+    create_table(connection, cursor) 
+    insert(connection, cursor)
+    disconnect_database(connection, cursor)
 
 if __name__ == "__main__":
     main()
